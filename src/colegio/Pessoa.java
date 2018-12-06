d03f0ca96a776fe225a5bd31964082f4db3be999
Aluno <yuri.otricolor@gmail.com>
2018-12-06 19:28:20 -0300

    Primeiro Commit

---
+++ src/colegio/Pessoa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colegio;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Pessoa {
+    
+    private String nome;
+    private Integer matricula;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Pessoa(String nome, Integer matricula, Integer idade) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+    }
+    
+}
